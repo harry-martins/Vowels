ab68152f300491b221077b242b5b9a943f51c656
harry-martins <52189030+harry-martins@users.noreply.github.com>
2019-07-23 15:42:38 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,8 @@
+
+x=input()
+if(x=='!' or x=='@' or x=='#' or x=='$' or x=='%' or x=='^' or x=='&' or x=='*'):
+	print("invalid")
+elif(x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
+	print("Vowel")
+else:
+	print("Consonant")
